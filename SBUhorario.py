d0beb77295ab2b43c728e297d79e886894274c56
Omarhdez-218 <130847536+Omarhdez-218@users.noreply.github.com>
2026-01-01 18:30:57 -0500

    Add comments & fix passday assignment logic
    Line 69, instead of randomly choosing between “passday1” or “passday2”, workers will simply just be assigned “passday”. This prevents someone being assigned “passday2” twice

--- SBUhorario.py
+++ SBUhorario.py
@@ -1,9 +1,11 @@
 
 import random
 
+# List of pods
 pods = ["B", "C", "D", "E", "F", "G",
         "I", "J", "K", "L", "breaker1", "breaker2"]
 
+#List of arbitrary's workers
 workers = [
     "worker1",  #Bill
     "worker2",  #Sue
@@ -21,10 +23,13 @@ workers = [
     "worker14", #newGirl
 ]
 
+# List of workers that can't be assigned pod "K"
 float_workers = set(["worker11", "worker12", "worker13", "worker14"])
 
+# Shuffling items in Pods list
 random.shuffle(pods)
 
+# Assigning fixed days off for each worker
 worker_passdays = {
     "worker1": [4, 9],
     "worker2": [2, 7],
@@ -42,27 +47,33 @@ worker_passdays = {
     "worker14": [9, 11]
 }
 
+# Track used pods & add them to a set
 used_pods = {}
 for w in workers:
     used_pods[w] = set()
 
+# Empty dictionary that will add each worker a unique position for each day
 schedule = {}
 
+
 for day in range(14):
+    # Rotating pods
     daily_pods = pods[day:] + pods[:day]
     daily_assignment = {}
     pod_index = 0
 
     for worker in workers:
 
-        # Pass day
+        # Assign passday to workerd based on workers_passday dict
         if (day + 1) in worker_passdays[worker]:
-            daily_assignment[worker] = random.choice(["passday1", "passday2"])
+            daily_assignment[worker] = "passday"
             continue
 
+        # Checks if a worker is already assigned a specific pod
         assigned = False
 
         for i in range(len(daily_pods)):
+            # Prevents daily_pods being out of index
             candidate = daily_pods[pod_index % len(daily_pods)]
             pod_index = pod_index + 1
 
@@ -74,7 +85,7 @@ for day in range(14):
                     assigned = True
                     break
                 continue
-
+                    
             if candidate not in used_pods[worker]:
                 daily_assignment[worker] = candidate
                 used_pods[worker].add(candidate)
